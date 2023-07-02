661caa0392bfd94520fb85586ef883bd152467c1
illyum <90023277+itzilly@users.noreply.github.com>
2023-07-02 01:10:13 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Update and upgrade system
+sudo apt update
+sudo apt upgrade -y
+
+# Install NGINX
+sudo apt install nginx -y
+
+# Install dependencies for Ruby
+sudo apt install git curl libssl-dev libreadline-dev zlib1g-dev libsqlite3-dev -y
+
+# Install rbenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+# Install ruby-build plugin
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# Install Ruby 3.2.2
+rbenv install 3.2.2
+rbenv global 3.2.2
+
+# Install bundler and project dependencies
+gem install bundler
+rbenv rehash
+bundle install
+
+# Open port 8080
+sudo ufw allow 8080
+
+# Clone the repository
+git clone https://github.com/ProudCircle/WebServer.git
+cd WebServer
+
+# Run the web server on port 8080
+rails server -p 8080
